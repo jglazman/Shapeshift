6eac0994f0259848b84c650a1201da26ab54e4cb
Jeremy Glazman <jeremy@gram.gs>
2020-04-28 21:32:05 +0100

    add 'noClipping' option to UserInput to prevent tapping through popups

--- ShapeshiftClient/Assets/Scripts/System/UserInput.cs
+++ ShapeshiftClient/Assets/Scripts/System/UserInput.cs
@@ -28,6 +28,9 @@ namespace Glazman.Shapeshift
 				UnityGuiPointerEvent = new PointerEventData(UnityEventSystem);
 		}
 
+		/// <summary>Raycast into the world to pick all active interactable objects in the scene.</summary>
+		/// <param name="screenPos">The point on the screen to pick from.</param>
+		/// <returns>A list of all GameObjects found, or an empty list if none were found.</returns>
 		public static IEnumerable<GameObject> PickObjects(Vector2 screenPos)
 		{
 			Assert.IsNotNull(UnityEventSystem, "[UserInput] Failed to pick: EventSystem is missing");
@@ -41,12 +44,20 @@ namespace Glazman.Shapeshift
 			return results.Select(r => r.gameObject);
 		}
 		
-		public static T PickObject<T>(Vector2 screenPos) where T : UnityEngine.Component
+		/// <summary>Raycast into the world to pick an active interactable MonoBehaviour of the given type.</summary>
+		/// <param name="screenPos">The point on the screen to pick from.</param>
+		/// <param name="noClipping">If true then clip through all objects to find a match, else stop at the first GameObject tagged with "ConsumeInput".</param>
+		/// <typeparam name="T">The type of MonoBehaviour we are trying to pick.</typeparam>
+		/// <returns>The picked MonoBehaviour, else null if none were found.</returns>
+		public static T PickObject<T>(Vector2 screenPos, bool noClipping=false) where T : UnityEngine.Component
 		{
 			var pickedObjects = PickObjects(screenPos);
 
 			foreach (var obj in pickedObjects)
 			{
+				if (!noClipping && obj.CompareTag("ConsumeInput"))
+					return null;	// consumed
+				
 				var component = obj.GetComponent<T>();
 				if (component != null)
 					return component;
