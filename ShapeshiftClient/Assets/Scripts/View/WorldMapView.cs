b9acb8d9057517fa5ae52795accb5834e1f1d306
Jeremy Glazman <jeremy@gram.gs>
2020-05-01 18:27:34 +0100

    persist the worldmap scroll view.  add some parallax.

--- ShapeshiftClient/Assets/Scripts/View/WorldMapView.cs
+++ ShapeshiftClient/Assets/Scripts/View/WorldMapView.cs
@@ -2,15 +2,54 @@
 // Copyright (c) 2020 Jeremy Glazman
 //
 
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Glazman.Shapeshift
 {
-	public class WorldMapView : MonoBehaviour
+	[Serializable]
+	public struct WorldMapScrollData
 	{
+		public Vector2 scrollOffset;
+	}
+	
+	public class WorldMapView : MonoBehaviour, ISceneTransitioner
+	{
+		[SerializeField] private ScrollRect _scrollRect;
 
+		
+		private void Awake()
+		{
+			SceneController.RegisterTransitioner(this);
+		}
 
+		private void OnDestroy()
+		{
+			SceneController.UnregisterTransitioner(this);
+		}
 
+		
+		public void Notify(TransitionState state)
+		{
+			switch (state)
+			{
+				case TransitionState.Intro:
+					_scrollRect.content.anchoredPosition = Database.Load<WorldMapScrollData>(0).Value.scrollOffset;
+					break;
+				
+				case TransitionState.Outro:
+					var data = Database.Load<WorldMapScrollData>(0);
+					data.Value.scrollOffset = _scrollRect.content.anchoredPosition;
+					Database.Save(data);
+					
+					SceneController.UnregisterTransitioner(this);
+					break;
+			}
+		}
+		
+
+		
 		public void OnClick_OpenDebugMenu()
 		{
 			PopupViewController.Open<WorldMapDebugPopup>();
